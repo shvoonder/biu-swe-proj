c228b749dce8937a5fea72c82a3bd56d38c3c391
Ronen <shvoonder@gmail.com>
2019-03-24 16:17:36 +0200

    update

--- proj/src/DB/DBconnection.java
+++ proj/src/DB/DBconnection.java
@@ -20,7 +20,7 @@ public class DBconnection {
         String sqlCreateSubTasks = "CREATE TABLE `SE_proj`.`sub_tasks` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `name` VARCHAR(3000) NOT NULL , `priority_id` INT NOT NULL , `user_id` INT NOT NULL , `skill_id` INT NOT NULL , `task_id` INT NOT NULL , PRIMARY KEY (`id`))";
         String sqlCreateTasks = "CREATE TABLE `SE_proj`.`tasks` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `name` VARCHAR(30) NOT NULL , `priority_id` INT NOT NULL , `user_id` INT NOT NULL , `skill_id` INT NOT NULL , `project_id` INT NOT NULL , PRIMARY KEY (`id`))";
         String sqlCreateUserAuth = "CREATE TABLE `SE_proj`.`user_auth` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `user_name` VARCHAR(200) NOT NULL , `password` VARCHAR(30) NOT NULL , `user_id` INT(30) NOT NULL , PRIMARY KEY (`id`))";
-        String sqlCreateUsers = "CREATE TABLE `SE_proj`.`users` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(30) NOT NULL , `sure_name` VARCHAR(30) NOT NULL , `user_name` VARCHAR(30) NOT NULL , `email` VARCHAR(50) NOT NULL , `is_admin` BOOLEAN NOT NULL , PRIMARY KEY (`id`))";
+        String sqlCreateUsers = "CREATE TABLE `SE_proj`.`users` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(30) NOT NULL , `sure_name` VARCHAR(30) NOT NULL , `user_name` VARCHAR(30) NOT NULL , `email` VARCHAR(50) NOT NULL , `password` VARCHAR(50) NOT NULL,  `is_admin` BOOLEAN NOT NULL , PRIMARY KEY (`id`))";
 
         boolean flag = true;
         ResultSet rs = null;
@@ -48,13 +48,46 @@ public class DBconnection {
             stmtDB.executeUpdate(sqlCreateUserAuth);
             stmtDB.executeUpdate(sqlCreateUsers);
         }
-        
-
-
-        }
-
-
     }
+
+    public static ResultSet getUsersList () throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        Connection con = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+        Statement stmt=con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT  * FROM users");
+        return rs;
+    }
+
+    public static boolean checkIfUserExists(String username) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException{
+        ResultSet rs = getUsersList();
+        while (rs.next())
+            if (rs.getString("user_name").equals(username))
+                return true;
+        return false;
+    }
+
+    public static boolean createUser (String username, String password, boolean isAdmin, String firstName, String sureName, String email) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        Connection con = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+        Statement stmt=con.createStatement();
+        int numOfColEffected = 0;
+        if (!checkIfUserExists(username))
+            numOfColEffected = stmt.executeUpdate("INSERT INTO 'users' ('first_name', 'sure_name', 'user_name', 'email', 'password', 'is_admin') VALUES (\""+firstName+"\",\""+sureName+"\",\""+username+"\",\""+email+"\",\""+password+"\",\""+isAdmin+"\")");
+        if (numOfColEffected != 0)
+            return true;
+        return false;
+    }
+
+    public static boolean checkLogin (String userName, String password) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException{
+        ResultSet rs = getUsersList();
+        while (rs.next())
+            if (rs.getString("user_name").equals(userName) && rs.getString("password").equals(password))
+                return true;
+        return false;
+    }
+
+    public static
+}
 
 
 
