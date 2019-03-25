5408fe692ec47320e800cf92fb407104d629356c
Ronen <shvoonder@gmail.com>
2019-03-25 15:54:43 +0200

    projV2 created

---
+++ projV2/src/Model/User.java
@@ -0,0 +1,83 @@
+package Model;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String sureName;
+    private String userName;
+    private String password;
+    private String email;
+    private boolean is_admin;
+
+    public User(int id, String firstName, String sureName, String userName, String email, String password, boolean is_admin) {
+        this.id = id;
+        this.firstName = firstName;
+        this.sureName = sureName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.is_admin = is_admin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSureName() {
+        return sureName;
+    }
+
+    public void setSureName(String sureName) {
+        this.sureName = sureName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIs_admin() {
+        return is_admin;
+    }
+
+    public void setIs_admin(boolean is_admin) {
+        this.is_admin = is_admin;
+    }
+
+    public boolean loadUser(String username){
+
+    }
+
+}
---
+++ projV2/web/signup.jsp
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<title>Task Manegment Project</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+<body>
+<!-- Header -->
+<header class="w3-container w3-theme w3-padding" id="myHeader">
+  <div class="w3-center">
+  <h1 class="w3-xxxlarge w3-animate-bottom">Sign up</h1>
+</header>
+
+<!-- Modal -->
+<hr>
+<h2 class="w3-xlarge w3-animate-top w3-center">Fill in your details </h2>
+</div>
+
+<div style="margin-right:400px; margin-left:400px" class="center">
+<form class="w3-container w3-card-2" action=404.html method="post" id=login>
+  <h2>sign up</h2>
+  <div class="w3-section">
+    <label>first name</label>
+    <input class="w3-input"  name="first_name" type="text" required>
+    <label>last name</label>
+    <input class="w3-input"  name="last_name" type="text" required>
+    <label>Username</label>
+    <input class="w3-input"  name="user_name" type="text" required>
+    <label>Password</label>
+    <input class="w3-input"  name="password" type="password" required>
+    <label>Email</label>
+    <input class="w3-input"  name="Email" type="email" required>
+  </div>
+  <div class="w3-section">
+    <button class="w3-button w3-theme" type="submit" formmethod="post">sign up</button>
+  </div>
+  </div>
+</form>
+</div>
+<hr>
+</body>
+</html>
