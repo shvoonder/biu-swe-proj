625317998c8266223a8ff471c548466f147966b7
Ronen <shvoonder@gmail.com>
2019-03-25 10:10:41 +0200

    24.3 - day end

--- proj/src/DB/DBconnection.java
+++ proj/src/DB/DBconnection.java
@@ -2,6 +2,7 @@ package DB;
 
 import java.sql.*;
 import java.util.List;
+import java.util.ArrayList;
 
 public class DBconnection {
 
@@ -17,9 +18,9 @@ public class DBconnection {
         String sqlCreatePriorityTypes = "CREATE TABLE `SE_proj`.`priority_type` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `name` VARCHAR(30) NOT NULL , PRIMARY KEY (`id`))";
         String sqlCreateProjects = "CREATE TABLE `SE_proj`.`projects` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `name` VARCHAR(30) NOT NULL , `priority_id` INT(30) NOT NULL , `user_id` INT(30) NOT NULL , PRIMARY KEY (`id`))";
         String sqlCreateSkillTypes = "CREATE TABLE `SE_proj`.`skil_types` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `skill` VARCHAR(30) NOT NULL , PRIMARY KEY (`id`))";
-        String sqlCreateSubTasks = "CREATE TABLE `SE_proj`.`sub_tasks` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `name` VARCHAR(3000) NOT NULL , `priority_id` INT NOT NULL , `user_id` INT NOT NULL , `skill_id` INT NOT NULL , `task_id` INT NOT NULL , PRIMARY KEY (`id`))";
-        String sqlCreateTasks = "CREATE TABLE `SE_proj`.`tasks` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `name` VARCHAR(30) NOT NULL , `priority_id` INT NOT NULL , `user_id` INT NOT NULL , `skill_id` INT NOT NULL , `project_id` INT NOT NULL , PRIMARY KEY (`id`))";
-        String sqlCreateUserAuth = "CREATE TABLE `SE_proj`.`user_auth` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `user_name` VARCHAR(200) NOT NULL , `password` VARCHAR(30) NOT NULL , `user_id` INT(30) NOT NULL , PRIMARY KEY (`id`))";
+        String sqlCreateSubTasks = "CREATE TABLE `SE_proj`.`sub_tasks` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `sub_task` VARCHAR(300) NOT NULL , `priority_id` INT NOT NULL , `user_id` INT NOT NULL ,  `task_id` INT NOT NULL , PRIMARY KEY (`id`))";
+        String sqlCreateTasks = "CREATE TABLE `SE_proj`.`tasks` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `task` VARCHAR(300) NOT NULL , `priority_id` INT NOT NULL , `user_id` INT NOT NULL , `skill_id` INT NOT NULL , `project_id` INT NOT NULL , PRIMARY KEY (`id`))";
+        String sqlCreateUserAuth = "CREATE TABLE `SE_proj`.`user_auth` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `user_name` VARCHAR(30) NOT NULL , `password` VARCHAR(30) NOT NULL , `user_id` INT(30) NOT NULL , PRIMARY KEY (`id`))";
         String sqlCreateUsers = "CREATE TABLE `SE_proj`.`users` ( `id` INT(30) NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(30) NOT NULL , `sure_name` VARCHAR(30) NOT NULL , `user_name` VARCHAR(30) NOT NULL , `email` VARCHAR(50) NOT NULL , `password` VARCHAR(50) NOT NULL,  `is_admin` BOOLEAN NOT NULL , PRIMARY KEY (`id`))";
 
         boolean flag = true;
@@ -54,7 +55,7 @@ public class DBconnection {
         Class.forName("com.mysql.jdbc.Driver").newInstance();
         Connection con = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
         Statement stmt=con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT  * FROM users");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM users");
         return rs;
     }
 
@@ -86,13 +87,55 @@ public class DBconnection {
         return false;
     }
 
-    public static boolean addTask (String userID, String task, ) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException{
+    public static boolean addTask (String userID, String task, int priorityID, int skillID, int projectID) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException{
         Class.forName("com.mysql.jdbc.Driver").newInstance();
         Connection con = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
         Statement stmt=con.createStatement();
-        int numOfColEfected = 0;
-
+        int numOfColEffected = 0;
+        numOfColEffected = stmt.executeUpdate("INSERT INTO 'tasks' ('task','priority_id','user_id','skill_id','project_id') VALUES (\""+task+"\",\""+priorityID+"\",\""+userID+"\",\""+skillID+"\",\""+projectID+"\")");
+        if (numOfColEffected != 0)
+            return true;
+        return false;
     }
+
+
+    public static ResultSet getTask () throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        Connection con = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+        Statement stmt=con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM tasks");
+        return rs;
+    }
+
+    public static List getTaskByProject (int projectID) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        ResultSet rs = getTask();
+        List tasksLst = new ArrayList(); // Create an ArrayList object
+        while (rs.next())
+            if (rs.getString("project_id").equals(projectID))
+                tasksLst.add(rs.getString("task"));
+        return tasksLst;
+    }
+
+    public static boolean addSubTask (String subTask, int priorityID, int userID, int taskID) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        Connection con = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+        Statement stmt=con.createStatement();
+        int numOfColEffected = 0;
+        numOfColEffected = stmt.executeUpdate("INSERT INTO 'sub_tasks' ('sub_task','priority_id','user_id','task_id') VALUES (\""+subTask+"\",\""+priorityID+"\",\""+userID+"\",\""+taskID+"\")");
+        if (numOfColEffected != 0)
+            return true;
+        return false;
+    }
+
+    public static boolean getSubTask (String userID, String task, int priorityID, int skillID, int projectID) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        Connection con = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+        Statement stmt=con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT  * FROM sub_tasks");
+        return rs;
+    }
+
+
 }
 
 
